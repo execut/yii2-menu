c400bacfae3a533705096e746300af0dd9aae10a
eXeCUT <sexecut@gmail.com>
2020-02-25 14:15:39 +0300

    added links migration

---
+++ migrations/m200225_111232_addLinkField.php
@@ -0,0 +1,22 @@
+<?php
+
+use execut\yii\migration\Migration;
+use execut\yii\migration\Inverter;
+
+class m200225_111232_addLinkField extends Migration
+{
+    public function initInverter(Inverter $i)
+    {
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
